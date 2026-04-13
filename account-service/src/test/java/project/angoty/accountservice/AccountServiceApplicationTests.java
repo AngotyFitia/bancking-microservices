250dc9180f50c9b5258e3672841819e281a848f8
AngotyFitia <angoty.rabari@gmail.com>
2026-04-13 21:20:46 +0200

    chore(account-service): initial project setup for account-service

---
+++ account-service/src/test/java/project/angoty/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.angoty.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
