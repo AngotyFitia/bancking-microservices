8c7ff708e03bfb996a04d819b05f403475aee5ea
AngotyFitia <angoty.rabari@gmail.com>
2026-04-13 18:30:26 +0200

    chore: initial project setup

---
+++ customer-service/src/test/java/project/angoty/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.angoty.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
