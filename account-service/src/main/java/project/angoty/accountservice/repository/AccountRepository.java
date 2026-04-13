23df8cb1eb29cb25f723e77a0f612154f9940e2e
AngotyFitia <angoty.rabari@gmail.com>
2026-04-13 21:27:50 +0200

    feat(account-service): add AccountRepository for CRUD operations

---
+++ account-service/src/main/java/project/angoty/accountservice/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package project.angoty.accountservice.repository;
+
+import project.angoty.accountservice.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
+
